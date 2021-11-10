3703c32a2606ea977c8d1b731272f53599d5a2d6
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2021-11-10 17:11:04 +0530

    Time: 8 ms (78.73%), Space: 10 MB (57.08%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-         priority_queue<int, vector<int> , greater<int>> minh;
+    int partition(vector<int>&nums,int l,int r){
         
-         for(int i=0;i<nums.size();i++){
-             minh.push(nums[i]);
-             if(minh.size() > k)
-                 minh.pop();
+        int rnd = (rand() % (r - l + 1)) + l;
+        swap(nums[rnd] , nums[r]);
+        
+        int idx = l;
+        
+        for(int i = l ; i < r ; i++){
+           if(nums[i] < nums[r]){
+            swap(nums[i] , nums[idx]);
+            idx++;
+           }
+        }
+        swap(nums[idx] , nums[r]);
+        return idx;
+    }
+    
+    int quickSelect(vector<int>&nums,int l,int r,int k){
+        while(l <= r){
+           int p= partition(nums,l,r);
+           if(p == k)
+             return nums[p];
+           if(p < k)
+               return quickSelect(nums, p+1, r, k);
+           else
+               return quickSelect(nums, l, p-1, k);
          }
-        return minh.top();
+         return -1;
+    }
+    
+    int findKthLargest(vector<int>&nums, int k) {
+        int n = nums.size();
+        return quickSelect(nums,0,n-1,n-k);
     }
 };

16d961d8915ee8176c90408083c38810def89c73
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-01-28 10:04:57 +0530

    Time: 4 ms (97.85%), Space: 10 MB (58.38%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
-    int partition(vector<int>& nums , int l, int r) {
-        int pivot = nums[r];
-        int j = l;
-        for (int i=l;i<=r;i++) {
-            if (nums[i] < pivot) {
-                swap(nums[i], nums[j]);
-                j++;
-            }
-        }
-        
-        swap(nums[j], nums[r]);
-        return j;
-    }
-    
     
     int findKthLargest(vector<int>& nums, int k) {
+         priority_queue<int, vector<int>, greater<int>> minh;
         
-        if (k == 0 || nums.empty())
-            return -1;
+         for(int i=0;i<nums.size();i++){
+             minh.push(nums[i]);
+             if(minh.size()>k)
+                 minh.pop();
+         }
         
-  
-        int l = 0;
-        int r = nums.size()-1;
-        
-        k= nums.size()-k;
-        
-        while(l < nums.size() && r >= 0) {
-            int p = partition(nums, l, r);
-            
-            if (p == k) 
-                return nums[p];
-            else if (p > k) 
-                r = p-1;
-             else 
-                l = p+1;
-            
-        }
-        
-        return -1;
+        return minh.top();
     }
 };

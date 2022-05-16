f30be0a2e99064be9617a14fc893aa28814a8a18
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-05-16 12:28:40 +0530

    Time: 603 ms (15.28%), Space: 17.2 MB (34.83%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, vector<int>&memo, int i){
-        if(i >= nums.size()-1) 
-            return 0;
+//     int solve(vector<int>&nums, vector<int>&memo, int i){
+//         if(i >= nums.size()-1) 
+//             return 0;
         
-        if(memo[i]!=INT_MAX-1)
-            return memo[i];
+//         if(memo[i]!=INT_MAX-1)
+//             return memo[i];
         
-	    for(int j=1;j<=nums[i];j++)
-		    memo[i] = min(memo[i], 1+solve(nums,memo,i+j));   
+// 	    for(int j=1;j<=nums[i];j++)
+// 		    memo[i] = min(memo[i], 1+solve(nums,memo,i+j));   
         
-	    return memo[i];
-    }
+// 	    return memo[i];
+//     }
     
     int jump(vector<int>& nums) {
         
-        vector<int>memo(nums.size(), INT_MAX-1);
-        return solve(nums, memo, 0);
+        int n = nums.size();
+        
+        vector<int>dp(n, INT_MAX-1);    
+        dp[n-1] = 0;
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j=1;j<=nums[i];j++){
+                if(i+j<n)
+                    dp[i] = min(dp[i], 1+dp[i+j]);
+            }
+        }
+        
+        return dp[0];
     }
 };

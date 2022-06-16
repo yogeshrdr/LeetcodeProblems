fc668431111228c849a3506f87ee39b0d8ffcec8
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-06-16 18:06:27 +0530

    Time: 69 ms (23.15%), Space: 12.4 MB (58.82%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    void markIslands(vector<vector<char>> &grid, int x, int y){
-       if(x<0 || x>=grid.size() || y<0 || y>=grid[0].size() || grid[x][y]!='1')
-        return;
-
-       grid[x][y] = 2;
-
-       markIslands(grid,x+1,y);
-       markIslands(grid,x,y+1);
-       markIslands(grid,x-1,y);
-       markIslands(grid,x,y-1);
+    void markisland(vector<vector<char>>& grid, int i, int j ){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j] != '1')
+            return;
+        
+        grid[i][j] = 'x';
+        
+        markisland(grid, i+1, j);
+        markisland(grid, i-1, j);
+        markisland(grid, i, j+1);
+        markisland(grid, i, j-1);
     }
     
     
     int numIslands(vector<vector<char>>& grid) {
-
-        int no_of_islands = 0;
-
+        int count  = 0;
+        
         for(int i=0;i<grid.size();i++)
-            for(int j=0;j<grid[i].size();j++)
-                if(grid[i][j]=='1'){
-                    markIslands(grid,i,j);
-                    no_of_islands +=1;
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j] == '1'){
+                    markisland(grid, i, j);
+                    count+=1;
                 }
-
-        return no_of_islands;
+            }
+        
+        return count;
     }
 };

d4262c60d82d8f9546dfa4090d0bf3b4eab6c275
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-06-16 09:30:00 +0530

    Time: 52 ms (17.21%), Space: 22.9 MB (8.40%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        TreeNode * temp = NULL;
+        
+        while(root != NULL){
+            if(p->val >= root->val)
+                root = root->right;
+            else{
+                temp = root;
+                root  = root->left;
+            }
+        }
+        
+        return temp;
+    }
+};

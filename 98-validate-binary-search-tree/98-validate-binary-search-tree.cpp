abf3362e3dc194c5a7383c4ede3eed337a33e2cd
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-06-29 09:18:33 +0530

    Time: 23 ms (31.23%), Space: 21.7 MB (70.16%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,18 +11,17 @@
  */
 class Solution {
 public:
-    bool solve(TreeNode *root, long min, long max){
-		if(root == NULL)
-			return true;
-		  
-		if(root->val > min && root->val < max) 
-			return solve(root->left, min, root->val) && solve(root->right, root->val, max);
-		
-		return false;
-       
+    bool solve(TreeNode*root, long min , long max){
+        if(root==NULL)
+            return true;
+        
+        if(root->val > min && root->val < max)
+            return solve(root->left, min, root->val) && solve(root->right, root->val, max);
+        
+        return false;
     }
     
     bool isValidBST(TreeNode* root) {
-       return solve(root, LONG_MIN, LONG_MAX);
+        return solve(root, LONG_MIN ,LONG_MAX);
     }
 };

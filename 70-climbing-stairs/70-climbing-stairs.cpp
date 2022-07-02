4014061f5977edf2b103e4156825eccef3cd92cd
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-07-02 22:53:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (30.71%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
+     
         if(n==0)
             return 0;
         
@@ -9,14 +10,15 @@ public:
         
         vector<int> dp(n+1);
         
+        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         
         for(int i=3;i<n+1;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+           dp[i] = dp[i-1] + dp[i-2];
         }
         
         return dp[n];
-      
+        
     }
 };

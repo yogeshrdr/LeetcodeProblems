b97bf94cc2896bec8abecddecc7b65490c292068
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-07-03 08:36:00 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.26%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,6 +4,14 @@ public:
         if(n<=1)
             return n;
         
-        return fib(n-1) + fib(n-2);
+        vector<int> dp(n+1);
+        
+        dp[1] = 1;
+        
+        for(int i=2;i<n+1;i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
+        
     }
 };

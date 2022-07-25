039a88779a9ce06d2e7ffded0ac867f5c95fdacc
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-07-25 23:43:32 +0530

    Time: 286 ms (21.54%), Space: 19.8 MB (76.01%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -23,15 +23,14 @@ public:
         for(int i=0;i<grid.size();i++){
             if(flag == true)
                 break;
-            
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
                     solve(grid, i, j);
+                    
                     flag = true;
+                    break;
                 }
                 
-                if(flag == true)
-                    break;
             }
             
             

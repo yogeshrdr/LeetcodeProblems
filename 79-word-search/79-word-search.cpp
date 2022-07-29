3d31889ed6c6dadb9befac38bd1cfc88099ffc7e
YOGESH YADAV <70858211+yogeshrdr@users.noreply.github.com>
2022-07-29 17:39:02 +0530

    Time: 1008 ms (19.57%), Space: 8 MB (35.71%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    
-     bool solve(vector<vector<char>>&board, string word, int i, int j, int n) {
-         
-        if(n == word.size()) 
+    bool solve(vector<vector<char>>&board, string word, int i, int j, int k){  
+        if(k == word.size()) 
             return true; 
         
-        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) 
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[k]) 
             return false;
         
-        board[i][j] = '-';
+        char x = board[i][j];
         
-        bool ans =  solve(board, word, i+1, j, n+1) ||  
-                    solve(board, word, i, j+1, n+1) || 
-                    solve(board, word, i-1, j, n+1) || 
-                    solve(board, word, i, j-1, n+1); 
+        board[i][j] = '*';
         
-
-        board[i][j] = word[n];
+        bool x1 = solve(board, word, i+1, j, k+1);
+        bool x2 = solve(board, word, i, j+1, k+1);
+        bool x3 = solve(board, word, i-1, j, k+1); 
+        bool x4 = solve(board, word, i, j-1, k+1);
+     
+        board[i][j] = x;
 		
-        return ans;
+        return x1 || x2 || x3 || x4;
+        
     }
+    
     
     bool exist(vector<vector<char>>& board, string word) {
         
-       if(word == "") 
-           return false;
-        
-        for(int i = 0; i < board.size(); i++) 
-            for(int j = 0; j < board[i].size(); j++) 
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
                 if(board[i][j] == word[0] && solve(board, word, i, j, 0))
                     return true;
+            }
+        }
         
         return false;
     }
